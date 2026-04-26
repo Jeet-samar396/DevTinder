c33e048ca5c1db8e9052314793ab7b9e002cb7cf
jeet-samar <mailsamar03@gmail.com>
2026-04-26 14:55:21 +0530

    frontend ready for deploy

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+import { BASE_URL } from "./constants";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
-  withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true, // 🔥 MUST
 });
 
 export default api;
